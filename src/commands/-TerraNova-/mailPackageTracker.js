3deeb7c442a649d26d667075d5dc88c486919fc4
TecnoRed <tecnored20@gmail.com>
2025-03-31 23:29:28 +0100

    Added new command to list the packets being tracked for the user

--- src/commands/-TerraNova-/mailPackageTracker.js
+++ src/commands/-TerraNova-/mailPackageTracker.js
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder, CommandInteraction, EmbedBuilder } from 'discord.js';
 import MailPackageTracker from '../../models/mailPackageTracker'
 import startTracking from '../../utils/mailPackageTracker';
 import eLog from '../../utils/eLog';
@@ -34,6 +34,10 @@ module.exports = {
         .setDescription('Codigo del Paquete')
         .setRequired(true)
       )
+    )
+    .addSubcommand(list =>
+      list.setName('list')
+      .setDescription('Lista los repartos activos del usuario')
     ),
   
   /**
@@ -83,6 +87,76 @@ module.exports = {
       clearInterval(intervalId)
       return await interaction.reply({
         content: `Se ha detenido el seguimiento del paquete: ${mptInstance.packageId}.`,
+        ephemeral: true,
+      });
+    }
+    else if(subCommand === "list") {
+      // TODO: Implementar una estrctura para retornar la lista de paquetes en seguimiento 
+
+      const filters = {
+        userId,
+        intervalId: { $ne: null } // Para obtener solo las activas
+      }
+
+      const allPackageActive = await MailPackageTracker.find(filters);
+      const processDataPackage = allPackageActive.map(packageActive => {
+        const companyId = packageActive.companyId;
+        const packageId = packageActive.packageId;
+
+        switch (companyId) {
+          case "correos":
+            return {
+              company: "Correos",
+              message: `Paquete: [${packageId}](https://www.correos.es/es/es/herramientas/localizador/envios/detalle?tracking-number=${packageId})`
+            }
+      
+          case "ecoscooting":
+            return {
+              company: "EcoScooting",
+              message: `Paquete: [${packageId}](https://www.ecoscooting.com/tracking/${packageId})`
+            }
+      
+          case "cttexpress":
+            return {
+              company: "CTT Express",
+              message: `Paquete: [${packageId}](https://www.cttexpress.com/localizador-de-envios/?sc=${packageId})`
+            }
+      
+          default:
+            return {
+              company: "Error",
+              message: "Ha ocurrido un error inesperado."
+            }
+        }
+      })
+      const filterPackage = processDataPackage.filter(x => x.company !== "Error");
+
+      const groupedPackage = Object.values(filterPackage.reduce((acc, { company, message }) => {
+        acc[company] = acc[company] || { company, message: new Set() };
+        acc[company].message.add(message);
+        return acc;
+      }, {})).map(({ company, message }) => ({ company, message: [...message] }));
+
+      const embed = new EmbedBuilder()
+        .setTitle("Paquetes en seguimiento")
+        .setColor("#0099ff");
+
+      groupedPackage.forEach(({ company, message }) => {
+        embed.addFields({
+            name: `📢 ${company}`,
+            value: message.map(msg => `- ${msg}`).join("\n"),
+            inline: false
+        });
+      });
+
+      if(client) {
+        const user = await client.users.fetch(userId);
+        const mdChannel = await user.createDM(true);
+        mdChannel.send({ embeds: [embed] })
+      }
+
+      return await interaction.reply({
+        content: `Revisa el DM`,
         ephemeral: true,
       });
     }
